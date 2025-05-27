2a4244e524c9e031120a8de2b7d12676225ce0a6
charltonxqh <csiaw001@e.ntu.edu.sg>
2025-05-27 20:41:38 +0800

    Revert "try to comment opik"
    This reverts commit ef654b7d044aed5283fa2ffd9ba42e18211bbbac.

--- evaluate.py
+++ evaluate.py
@@ -1,24 +1,24 @@
-# import asyncio
-# import logging
-# from utils import get_api_key
-# from chatbot import NDII
-# import config
+import asyncio
+import logging
+from utils import get_api_key
+from chatbot import NDII
+import config
 
-# # Setup logging
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger("NDII-Evaluate")
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("NDII-Evaluate")
 
-# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
                               
-# async def main():
-#     # Get API key
-#     api_key = get_api_key()
+async def main():
+    # Get API key
+    api_key = get_api_key()
     
-#     # Initialize NDII with database
-#     logger.info("Creating NDII instance...")
-#     ndii = await NDII.create_db(api_key, max_history=4, rag_config=config.RAG)
+    # Initialize NDII with database
+    logger.info("Creating NDII instance...")
+    ndii = await NDII.create_db(api_key, max_history=4, rag_config=config.RAG)
 
-#     await ndii.evaluate()
+    await ndii.evaluate()
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
